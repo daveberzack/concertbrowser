8326f7d8695e177a1a3f3b6c23f16344db411038
Dave Berzack <daveberzack@gmail.com>
2022-01-27 10:10:56 -0500

    Updates

---
+++ precache-manifest.381cf2bc0113d8d40bfbeb5291dc94b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b637ef39ee47a5ef082c5a5284b1d4",
+    "url": "/concertbrowser/index.html"
+  },
+  {
+    "revision": "be180bb8fe08d315d1ee",
+    "url": "/concertbrowser/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e5341683a40bc26b689a",
+    "url": "/concertbrowser/static/css/main.2e1c8465.chunk.css"
+  },
+  {
+    "revision": "be180bb8fe08d315d1ee",
+    "url": "/concertbrowser/static/js/2.1a07b353.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/concertbrowser/static/js/2.1a07b353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5341683a40bc26b689a",
+    "url": "/concertbrowser/static/js/main.6bedbb14.chunk.js"
+  },
+  {
+    "revision": "ec46a724212ef4c35e47",
+    "url": "/concertbrowser/static/js/runtime-main.8ede6429.js"
+  }
+]);
